566c722e679b43c5f24b5171a2a723f619f89177
Joanna Kahloun <arya.miranda.arya@gmail.com>
2022-04-22 19:10:20 +0200

    First card design

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let currentPokemon;
 
 
 async function loadPokemon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
+    let url = 'https://pokeapi.co/api/v2/pokemon/1';
     let response = await fetch(url);
     let currentPokemon = await response.json();
     console.log('Loaded pokemon', currentPokemon);
@@ -12,6 +12,11 @@ async function loadPokemon() {
 }
 
 function renderPokemonInfo(currentPokemon) {
-    document.getElementById('pokemon-name').innerHTML = currentPokemon['name'];
+    let pokemonName = currentPokemon['name'];
+    let changedPokemonName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
+    document.getElementById('pokemon-name').innerHTML = changedPokemonName;
     document.getElementById('pokemon-img').src = currentPokemon['sprites']['other']['dream_world']['front_default'];
 }
+
+
+  
