dafce6a9b291170f1d405ad4931e54673e177d1c
Joanna Kahloun <arya.miranda.arya@gmail.com>
2022-04-29 19:07:07 +0200

    Progress bar and animation added

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ let pokemonLimit = 10;
 async function loadPokemon() {
 
     for (let i = 1; i < pokemonLimit; i++) {
-    let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    let response = await fetch(url);
-    currentPokemon = await response.json();
-    console.log('Loaded pokemon', currentPokemon);
-    renderPokemonCards(i);
+        let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        let response = await fetch(url);
+        currentPokemon = await response.json();
+        console.log('Loaded pokemon', currentPokemon);
+        renderPokemonCards(i);
     }
     renderPokemonInfo(i);
 }
@@ -22,20 +22,20 @@ async function loadPokemonInfo() {
         let response = await fetch(url);
         currentSpecies = await response.json();
         console.log('Loaded pokemon', currentSpecies);
-        }
+    }
     renderPokemonInfo(i);
 }
 
 
 function renderPokemonCards(i) {
-    
-        let img = currentPokemon['sprites']['other']['dream_world']['front_default'];
-        let pokemonName = currentPokemon['name'];
-        let changedPokemonName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
+
+    let img = currentPokemon['sprites']['other']['dream_world']['front_default'];
+    let pokemonName = currentPokemon['name'];
+    let changedPokemonName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
 
 
     document.getElementById('pokedex-container').innerHTML +=
-    `<div class="pokemon-card" style="background-color: var(--c-${currentPokemon['types'][0]['type']['name']})" >
+        `<div class="pokemon-card" style="background-color: var(--c-${currentPokemon['types'][0]['type']['name']})" >
     <div class="pokemon-card-top">
         <div>#${i}</div>
     </div>
@@ -50,7 +50,7 @@ function renderPokemonCards(i) {
             <img src="${img}" alt="">
         </div>
     </div>
-</div>` 
+</div>`
 }
 
 function getPokemonTypes() {
@@ -67,12 +67,13 @@ function renderPokemonInfo(i) {
     let pokemonName = currentPokemon['name'];
     let changedPokemonName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
     document.getElementById('pokemon-name').innerHTML = changedPokemonName;
-    document.getElementById('pokemon-id').innerHTML = '#'+ i;
+    document.getElementById('pokemon-id').innerHTML = '#' + i;
     document.getElementById('pokemon-type').innerHTML = getPokemonTypes();
-    document.getElementById('pokemon-height').innerHTML = currentPokemon.height/10 + " m";
-    document.getElementById('pokemon-weight').innerHTML = currentPokemon.weight/10 + " kg";
+    document.getElementById('pokemon-height').innerHTML = currentPokemon.height / 10 + " m";
+    document.getElementById('pokemon-weight').innerHTML = currentPokemon.weight / 10 + " kg";
     document.getElementById('pokemon-abilities').innerHTML = getAbilities();
     document.getElementById('info-text').innerHTML = currentSpecies['flavor_text_entries'][6]['flavor_text'];
+    document.getElementById('base-stats').innerHTML = getBaseStats();
 }
 
 
@@ -83,4 +84,26 @@ function getAbilities() {
         pokemonAbilities += ` <span>${ability}<br></span>`;
     }
     return pokemonAbilities;
+}
+
+function getBaseStats() {
+    let statsNames = ['HP', 'Attack', 'Defense', 'Sp. Atk.', 'Sp. Def.', 'Speed'];
+    let statsContent = "";
+    for (let i = 0; i < currentPokemon.stats.length; i++) {
+        statsContent += `
+            <div class="stats-names">
+                <div class="inline">
+                <div style="min-width: 100px;">
+                <b>${statsNames[i]} </b>
+                </div>
+                <div style="min-width: 35px;"><span> ${currentPokemon.stats[i].base_stat}</span>
+                </div> 
+                <div class="progress">
+                <span class="progress-bar" style="width: ${currentPokemon.stats[i].base_stat / 2}%"></span>
+              </div>
+              </div>
+            </div>
+        `
+    }
+    return statsContent;
 }
