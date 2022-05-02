90b29d35bf60f18458531c028aaf80dbdd76a4c4
JoannaKahloun <101954470+JoannaKahloun@users.noreply.github.com>
2022-05-02 20:05:28 +0200

    Improvements of onclick functions

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let currentPokemon;
 let currentSpecies;
 let pokemonLimit = 10;
+let allPokemonsData = [];
+let allPokomonsSpeciesData = [];
 
-
-async function loadPokemon() {
+async function loadPokemon(i) {
 
     for (let i = 1; i < pokemonLimit; i++) {
         let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
@@ -11,17 +12,21 @@ async function loadPokemon() {
         currentPokemon = await response.json();
         console.log('Loaded pokemon', currentPokemon);
         renderPokemonCards(i);
+        allPokemonsData.push(currentPokemon);
     }
     renderPokemonInfo(i);
 }
 
 
-async function loadPokemonInfo() {
+async function loadPokemonInfo(i) {
     for (let i = 1; i < pokemonLimit; i++) {
         let url = `https://pokeapi.co/api/v2/pokemon-species/${i}`;
+        let secondUrl = `https://pokeapi.co/api/v2/pokemon/${i}`;
         let response = await fetch(url);
+        let secondResponse = await fetch(secondUrl);
         currentSpecies = await response.json();
-        console.log('Loaded pokemon', currentSpecies);
+        currentPokemon = await secondResponse.json();
+        allPokomonsSpeciesData.push(currentSpecies);
     }
     renderPokemonInfo(i);
 }
@@ -33,9 +38,8 @@ function renderPokemonCards(i) {
     let pokemonName = currentPokemon['name'];
     let changedPokemonName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
 
-
     document.getElementById('pokedex-container').innerHTML +=
-        `<div class="pokemon-card" style="background-color: var(--c-${currentPokemon['types'][0]['type']['name']})" >
+        `<div onclick="openInfoCard(${i})" class="pokemon-card" style="background-color: var(--c-${currentPokemon['types'][0]['type']['name']})" >
     <div class="pokemon-card-top">
         <div class="pokemon-id">#${i}</div>
     </div>
@@ -43,7 +47,7 @@ function renderPokemonCards(i) {
         <div class="pokemon-info-left">
             <span id="pokemon-name">${changedPokemonName}</span>
             <div class="pokemon-type">
-            ${getPokemonTypes()}
+            ${loadPokemonTypes()}
             </div>
         </div>
         <div class="img-container">
@@ -53,7 +57,8 @@ function renderPokemonCards(i) {
 </div>`
 }
 
-function getPokemonTypes() {
+
+function loadPokemonTypes() {
     let pokemonType = "";
     for (i = 0; i < currentPokemon.types.length; i++) {
         type = currentPokemon['types'][i]['type']['name'];
@@ -63,45 +68,57 @@ function getPokemonTypes() {
 }
 
 
+function getPokemonTypes() {
+    let pokemonType = "";
+    for (i = 0; i < allPokemonsData[i].types.length; i++) {
+        type = allPokemonsData[i]['types'][i]['type']['name'];
+        pokemonType += ` <span class="type-info">${type}</span>`;
+    }
+    return pokemonType;
+}
+
+
 function renderPokemonInfo(i) {
-    let pokemonName = currentPokemon['name'];
+    i = i - 1;
+    let pokemonName = allPokemonsData[i]['name'];
     let changedPokemonName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
     document.getElementById('pokemon-name').innerHTML = changedPokemonName;
-    document.getElementById('pokemon-id').innerHTML = '#' + i;
+    document.getElementById('pokemon-id').innerHTML = '#' + Number(i+1);
     document.getElementById('pokemon-type').innerHTML = getPokemonTypes();
-    document.getElementById('pokemon-height').innerHTML = currentPokemon.height / 10 + " m";
-    document.getElementById('pokemon-weight').innerHTML = currentPokemon.weight / 10 + " kg";
-    document.getElementById('pokemon-abilities').innerHTML = getAbilities();
-    document.getElementById('info-text').innerHTML = currentSpecies['flavor_text_entries'][6]['flavor_text'];
+    document.getElementById('pokemon-height').innerHTML = allPokemonsData[i].height / 10 + " m";
+    document.getElementById('pokemon-weight').innerHTML = allPokemonsData[i].weight / 10 + " kg";
+    document.getElementById('pokemon-abilities').innerHTML = loadAbilities();
+    document.getElementById('info-text').innerHTML = allPokomonsSpeciesData[i]['flavor_text_entries'][6]['flavor_text'];
     document.getElementById('base-stats').innerHTML = getBaseStats();
-    document.getElementById('info-img').src = currentPokemon['sprites']['other']['dream_world']['front_default'];
-    document.getElementById('pokemon-info-card').style = `background-color: var(--c-${currentPokemon['types'][0]['type']['name']})`
+    document.getElementById('info-img').src = allPokemonsData[i]['sprites']['other']['dream_world']['front_default'];
+    document.getElementById('pokemon-info-card').style = `background-color: var(--c-${allPokemonsData[i]['types'][0]['type']['name']})`
 }
 
 
-function getAbilities() {
-    let pokemonAbilities = "";
-    for (i = 0; i < currentPokemon.abilities.length; i++) {
-        ability = currentPokemon['abilities'][i]['ability']['name'];
-        pokemonAbilities += ` <span>${ability}<br></span>`;
+function loadAbilities() {
+    let abilities = "";
+    for (i = 0; i < allPokemonsData[i].abilities.length; i++) {
+        ability = allPokemonsData[i]['abilities'][i]['ability']['name'];
+        abilities += ` <span>${ability}<br></span>`;
     }
-    return pokemonAbilities;
+    return abilities;
 }
+
 
 function getBaseStats() {
     let statsNames = ['HP', 'Attack', 'Defense', 'Sp. Atk.', 'Sp. Def.', 'Speed'];
     let statsContent = "";
-    for (let i = 0; i < currentPokemon.stats.length; i++) {
+    for (let i = 0; i < allPokemonsData[i].stats.length; i++) {
         statsContent += `
             <div class="stats-names">
                 <div class="inline">
                 <div style="min-width: 100px;">
-                <b>${statsNames[i]} </b>
+                <b class="stats-name">${statsNames[i]} </b>
                 </div>
-                <div style="min-width: 35px;"><span> ${currentPokemon.stats[i].base_stat}</span>
+                <div style="min-width: 35px;"><span><b> ${allPokemonsData[i].stats[i].base_stat}</b></span>
                 </div> 
                 <div class="progress">
-                <span class="progress-bar" style="width: ${currentPokemon.stats[i].base_stat / 2}%"></span>
+                <span class="progress-bar" style="width: ${allPokemonsData[i].stats[i].base_stat / 2}%"></span>
               </div>
               </div>
             </div>
@@ -130,6 +147,13 @@ function openInfoAbout() {
     document.getElementById('about-info').classList.remove('d-none');
 }
 
+
 function closeInfoCard() {
-    document.getElementById('info-card-container').style = "display: none";
+    document.getElementById(`info-card-container`).classList.add('d-none');
+}
+
+
+function openInfoCard(i) {
+    loadPokemonInfo(i);
+    document.getElementById(`info-card-container`).classList.remove('d-none');
 }
